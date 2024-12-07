332c2c14828e7a73d1556925c32952a4c5165043
rxv4 <zq.5rev5@gmail.com>
2024-12-07 03:39:43 +0100

    initial files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="content">
-    <h1>Projet site par les enfants</h1>
+    <h1>/rxv4/</h1>
   </div>
 </template>
 
 <style>
-/* Supprime les marges globales */
 body, html {
   margin: 0;
   padding: 0;
   height: 100%;
 }
 
-/* Applique le fond noir à tout l'écran */
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: rgb(11, 10, 17);
-  color: white;
+  color: rgb(200,200,200);
   height: 100vh;
   margin: 0;
 }
