77b5cb4d82f9ae8b0e4def08cbba3a1c0b397ea7
rxv4 <zq.5rev5@gmail.com>
2025-05-20 12:37:44 +0200

    gc

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue';
 import router from './router';
 import "./assets/global.css";
 
+const app = createApp(App);
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+//goatcounter
+router.afterEach(() => {
+  if (window.goatcounter && typeof window.goatcounter.count === 'function') {
+    window.goatcounter.count({
+      path: location.pathname + location.search + location.hash,
+    });
+  }
+});
+
+app.use(router).mount('#app');
